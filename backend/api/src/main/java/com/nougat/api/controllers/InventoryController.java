ff2eed6d03151eddf556475f47bd922d3e8a64bb
Robt291610 <158675065+Robt291610@users.noreply.github.com>
2026-04-05 18:23:03 -0400

    get inventory by id endpoint

--- backend/api/src/main/java/com/nougat/api/controllers/InventoryController.java
+++ backend/api/src/main/java/com/nougat/api/controllers/InventoryController.java
@@ -2,21 +2,26 @@ package com.nougat.api.controllers;
 
 import com.nougat.application.dto.inventory.InventorySummaryDTO;
 import com.nougat.application.usecases.inventory.queries.GetAllInventoryQuery;
+import com.nougat.application.usecases.inventory.queries.GetInventoryByIdQuery;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("api/inventory")
 public class InventoryController {
 
     private final GetAllInventoryQuery getAll;
+    private final GetInventoryByIdQuery getById;
 
-    public InventoryController(GetAllInventoryQuery getAll) {
+    public InventoryController(GetAllInventoryQuery getAll, GetInventoryByIdQuery getById) {
         this.getAll = getAll;
+        this.getById = getById;
     }
 
     @GetMapping
@@ -25,5 +30,9 @@ public class InventoryController {
         return getAll.handle();
     }
 
-
+    @GetMapping("{id}")
+    @Operation(summary = "Get inventory by id")
+    public InventorySummaryDTO getById(@PathVariable UUID id){
+        return getById.handle(id);
+    }
 }
